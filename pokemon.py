ba6b79a38a7feb0f500a1e4bf29773fb19f3ef5b
Tal Ornan <76403961+talornan@users.noreply.github.com>
2022-01-10 10:31:28 +0200

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -45,14 +45,5 @@ class Pokemons(object):
         this class is in use in order to simplify  the use in the json that return from the client
     """
 
-    def __init__(self, pokemons_json_str, graph):
-        if type(pokemons_json_str) is str:
-
-            self.pokemons = []
-            pokemons_json = json.loads(pokemons_json_str)
-            if type(pokemons_json) is not int and type(pokemons_json["Pokemons"]) is not int:
-                for pokemon in pokemons_json["Pokemons"]:
-                    self.pokemons.append(Pokemon(pokemon["Pokemon"], graph))
-
-
+    
 
